77b9028300485702414543619137b89cd8ba4a55
estel <estel@DESKTOP-44S7CP6>
2021-02-06 16:51:03 +0100

    Ajout classe Photo

--- src/main/java/miage/bd/HibernateUtil.java
+++ src/main/java/miage/bd/HibernateUtil.java
@@ -36,6 +36,7 @@ public class HibernateUtil
                         
                         configuration.addAnnotatedClass(miage.metier.EAN.class);
                         configuration.addAnnotatedClass(miage.metier.Calendrier.class);
+                        configuration.addAnnotatedClass(miage.metier.Photo.class);
 
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
             System.out.println("Hibernate serviceRegistry created");
